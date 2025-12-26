783bb59ca5b8ef938e192973607f6e76ea794994
dsc <549252873@qq.com>
2025-12-26 16:47:47 +0800

    添加vivo应用市场

--- packages/flutter_app_publisher/lib/src/publishers/vivo/app_package_publisher_vivo.dart
+++ packages/flutter_app_publisher/lib/src/publishers/vivo/app_package_publisher_vivo.dart
@@ -5,6 +5,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_app_publisher/flutter_app_publisher.dart';
 import 'package:crypto/crypto.dart';
 import 'package:flutter_app_publisher/src/publishers/vivo/publish_vivo_config.dart';
+import 'package:parse_app_package/parse_app_package.dart';
 
 /// HONOR Connect publishing API doc
 /// [https://developer.honor.com/cn/doc/guides/101359#h2-1712046097865]
@@ -34,16 +35,18 @@ class AppPackagePublisherVivo extends AppPackagePublisher {
       file,
       onPublishProgress,
     );
+    print(uploadResult);
+    await updateApp(uploadResult['serialnumber'],publishConfig,file);
     return PublishResult(
       url: 'https://dev.vivo.com.cn/app/appService/166035',
     );
   }
 
   Future<Map<String, dynamic>> uploadFile(
-  PublishVivoConfig publishConfig,
-      File file,
-      PublishProgressCallback? onPublishProgress,
-      ) async {
+    PublishVivoConfig publishConfig,
+    File file,
+    PublishProgressCallback? onPublishProgress,
+  ) async {
     try {
       int timestamp = DateTime.now().millisecondsSinceEpoch;
       String fileMd5 = await getFileSha256(file);
@@ -56,7 +59,7 @@ class AppPackagePublisherVivo extends AppPackagePublisher {
         "format": "json",
         "target_app_key": "developer",
         'packageName': 'cn.sigo',
-        'fileMd5':fileMd5
+        'fileMd5': fileMd5
       };
       FormData formData = FormData.fromMap({
         'file': await MultipartFile.fromFile(file.path),
@@ -69,7 +72,7 @@ class AppPackagePublisherVivo extends AppPackagePublisher {
         'target_app_key': 'developer',
         'packageName': 'cn.sigo',
         'sign': getSign(params, publishConfig.clientSecret),
-        'fileMd5':fileMd5,
+        'fileMd5': fileMd5,
       });
       Response response = await _dio.post(
         'https://developer-api.vivo.com.cn/router/rest',
@@ -82,16 +85,71 @@ class AppPackagePublisherVivo extends AppPackagePublisher {
           onPublishProgress?.call(count, total);
         },
       );
-      if (response.statusCode == 200 && response.data['code'] == 0) {print('上传成功，等待下一步发布');
-       return response.data['data'];
+      if (response.statusCode == 200 && response.data['code'] == 0) {
+        print('上传成功，等待下一步发布');
+        print(response.data['data']);
+        return response.data['data'];
       } else {
-        throw PublishError('getAccessToken error: ${response.data}');
+        throw PublishError('upload error: ${response.data}');
       }
     } catch (e) {
       throw PublishError(e.toString());
     }
   }
 
+  Future<Map<String, dynamic>> updateApp(String apk,PublishVivoConfig publishConfig,File file) async{
+    int timestamp = DateTime.now().millisecondsSinceEpoch;
+    AppPackage appPackage = await parseAppPackage(file);
+    String fileMd5 = await getFileSha256(file);
+    Map<String, dynamic> params = {
+      "access_key": publishConfig.clientId,
+      "timestamp": timestamp.toString(),
+      "method": "app.sync.update.app",
+      "v": "1.0",
+      "sign_method": "HMAC-SHA256",
+      "format": "json",
+      "target_app_key": "developer",
+      'packageName': 'cn.sigo',
+      'versionCode': appPackage.version,
+      'apk':apk,
+      'fileMd5':fileMd5,
+      'onlineType':1,
+      'compatibleDevice':1,
+    };
+    Map<String, dynamic> formData = {
+      'method': 'app.sync.update.app',
+      'access_key': publishConfig.clientId,
+      'timestamp': timestamp.toString(),
+      'format': 'json',
+      'v': 1.0,
+      'sign_method': 'HMAC-SHA256',
+      'target_app_key': 'developer',
+      'packageName': 'cn.sigo',
+      'sign': getSign(params, publishConfig.clientSecret),
+      'versionCode': appPackage.version,
+      'apk':apk,
+      'fileMd5':fileMd5,
+      'onlineType':1,
+      'compatibleDevice':1,
+    };
+    try {
+      Response response = await _dio.post(
+        'https://developer-api.vivo.com.cn/router/rest',
+        data: formData,
+        options: Options(
+          contentType: 'application/x-www-form-urlencoded',
+          responseType: ResponseType.json,
+        ),
+      );
+      if (response.statusCode == 200 && response.data['code'] == 0) {
+        return Map<String, dynamic>.from(response.data);
+      } else {
+        throw PublishError('applyUpload error: ${response.data}');
+      }
+    } catch (e) {
+      throw PublishError('applyUpload error: ${e.toString()}');
+    }
+  }
 
   Future<String> getFileSha256(File file) async {
     List<int> fileBytes = await file.readAsBytes();
@@ -99,7 +157,7 @@ class AppPackagePublisherVivo extends AppPackagePublisher {
     return digest.toString();
   }
 
-  static String hmacSHA256(String data, String key) {
+   String hmacSHA256(String data, String key) {
     var keyBytes = utf8.encode(key);
     var dataBytes = utf8.encode(data);
     var hmac = Hmac(sha256, keyBytes);
@@ -122,8 +180,10 @@ class AppPackagePublisherVivo extends AppPackagePublisher {
 
     return paramList.join('&');
   }
-
-  static String getSign(Map<String, dynamic> params, String accessSecret,) {
+    String getSign(
+    Map<String, dynamic> params,
+    String accessSecret,
+  ) {
     String sign = hmacSHA256(getUrlParamsFromMap(params), accessSecret);
     return sign;
   }
