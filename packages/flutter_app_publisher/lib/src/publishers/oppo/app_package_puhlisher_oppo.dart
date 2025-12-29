f3080e5f444d57b7c9cc3670d7bd49246150b185
dsc <549252873@qq.com>
2025-12-29 17:25:48 +0800

    添加oppo

--- packages/flutter_app_publisher/lib/src/publishers/oppo/app_package_puhlisher_oppo.dart
+++ packages/flutter_app_publisher/lib/src/publishers/oppo/app_package_puhlisher_oppo.dart
@@ -1,15 +1,12 @@
-
 import 'dart:convert';
 import 'dart:io';
-import 'package:crypto/crypto.dart' ;
+import 'package:crypto/crypto.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_app_publisher/flutter_app_publisher.dart';
 import 'package:flutter_app_publisher/src/publishers/oppo/publish_oppo_config.dart';
 import 'package:parse_app_package/parse_app_package.dart';
 
-
 class AppPackagePublisherOppo extends AppPackagePublisher {
-
   final Dio _dio = Dio();
 
   @override
@@ -18,28 +15,27 @@ class AppPackagePublisherOppo extends AppPackagePublisher {
   @override
   List<String> get supportedPlatforms => ['android'];
 
-
   @override
   Future<PublishResult> publish(
-      FileSystemEntity fileSystemEntity, {
-        Map<String, String>? environment,
-        Map<String, dynamic>? publishArguments,
-        PublishProgressCallback? onPublishProgress,
-      }) async {
+    FileSystemEntity fileSystemEntity, {
+    Map<String, String>? environment,
+    Map<String, dynamic>? publishArguments,
+    PublishProgressCallback? onPublishProgress,
+  }) async {
     File file = fileSystemEntity as File;
     PublishAppOppoConfig publishConfig = PublishAppOppoConfig.parse(
       environment,
       publishArguments,
     );
 
-   String accessToken = await getAccessToken(publishConfig.clientId, publishConfig.clientSecret);
+    String accessToken = await getAccessToken(publishConfig.clientId, publishConfig.clientSecret);
     String timestamp = (DateTime.now().millisecondsSinceEpoch ~/ 1000).toString();
     Map<String, dynamic> data = await getUploadUrl(accessToken, publishConfig.clientSecret, timestamp);
-    Map<String, dynamic> uploadInfo = await uploadFile(accessToken, timestamp, data, publishConfig.clientSecret, file,onPublishProgress);
-    await updateApp(accessToken, timestamp, publishConfig.clientSecret,file,uploadInfo);
+    Map<String, dynamic> uploadInfo =
+        await uploadFile(accessToken, timestamp, data, publishConfig.clientSecret, file, onPublishProgress);
+    await updateApp(accessToken, timestamp, publishConfig.clientSecret, file, uploadInfo);
     return PublishResult(
-      url:
-      'https://open.oppomobile.com/new/mcom/app/detail?app_id=3497867&pkg_symbol=0',
+      url: 'https://open.oppomobile.com/new/mcom/app/detail?app_id=3497867&pkg_symbol=0',
     );
   }
 
@@ -55,7 +51,7 @@ class AppPackagePublisherOppo extends AppPackagePublisher {
       if (response.statusCode == 200 && response.data['data']['access_token'] != null) {
         return response.data['data']['access_token'];
       } else {
-         throw PublishError('getAccessToken error: ${response.data}');
+        throw PublishError('getAccessToken error: ${response.data}');
       }
     } catch (e) {
       throw PublishError(e.toString());
@@ -86,14 +82,8 @@ class AppPackagePublisherOppo extends AppPackagePublisher {
     }
   }
 
-  Future<Map<String, dynamic>> uploadFile(
-      String accessToken,
-      String timestamp,
-      Map<String, dynamic> uploadInfo,
-      String accessSecret,
-      File file,
-      PublishProgressCallback? onPublishProgress
-      ) async {
+  Future<Map<String, dynamic>> uploadFile(String accessToken, String timestamp, Map<String, dynamic> uploadInfo,
+      String accessSecret, File file, PublishProgressCallback? onPublishProgress) async {
     Map<String, dynamic> params = {
       'access_token': accessToken,
       'timestamp': timestamp,
@@ -125,24 +115,20 @@ class AppPackagePublisherOppo extends AppPackagePublisher {
     }
   }
 
-  Future<Map<String,dynamic>>updateApp(
-      String accessToken,
-      String timestamp,
-      String accessSecret,
-      File file,
-  Map<String, dynamic> data,
-      )async{
+  Future<Map<String, dynamic>> updateApp(
+    String accessToken,
+    String timestamp,
+    String accessSecret,
+    File file,
+    Map<String, dynamic> data,
+  ) async {
     AppPackage appPackage = await parseAppPackage(file);
     Map<String, dynamic> query = {
       'access_token': accessToken,
       'timestamp': timestamp,
-      'pkg_name':'cn.sigo',
-      'version_code':appPackage.buildNumber,
-      'apk_url':{
-        'url':data['url'],
-        'md5':await generateMd5(file),
-        'cpu_code':0
-      },
+      'pkg_name': 'cn.sigo',
+      'version_code': appPackage.buildNumber,
+      'apk_url': {'url': data['url'], 'md5': await generateMd5(file), 'cpu_code': 0},
       // 'app_name':'视客眼镜网',
       // 'second_category_id':6762,
       // 'third_category_id':6692,
@@ -150,13 +136,12 @@ class AppPackagePublisherOppo extends AppPackagePublisher {
       // 'detail_desc':'视客眼镜网 (sigo.cn)，一个专业的隐形眼镜购物平台，拥有自己的网站、APP和小程序。十多年来，视客对入驻平台的品牌资质严格把关，致力于为消费者带来舒心的购物体验。',
       // 'update_desc':'',
       // 'privacy_source_url':'https://m.vsigo.cn/privacyagreement',
-      'online_type':1,
-
+      'age_level':12,
+      'online_type': 1,
+      'adaptive_equipment':4,
     };
     String appsign = getSign(query, accessSecret);
-    query.addAll({
-      'api_sign': appsign
-    });
+    query.addAll({'api_sign': appsign});
     try {
       Response response = await _dio.post(
         'https://oop-openapi-cn.heytapmobi.com/resource/v1/app/upd',
@@ -167,7 +152,7 @@ class AppPackagePublisherOppo extends AppPackagePublisher {
       );
       print(response.data);
       if (response.data?['errno'] == 0) {
-        if(response.data['data']['success']){
+        if (response.data['data']['success']) {
           return Map<String, dynamic>.from(response.data['data']);
         }
         throw PublishError('updateapp error: ${response.data}');
@@ -204,17 +189,16 @@ class AppPackagePublisherOppo extends AppPackagePublisher {
   }
 
   String getSign(
-      Map<String, dynamic> params,
-      String accessSecret,
-      ) {
+    Map<String, dynamic> params,
+    String accessSecret,
+  ) {
     String sign = hmacSHA256(getUrlParamsFromMap(params), accessSecret);
     return sign;
   }
 
-  static Future<String> generateMd5(File file) async{
+  static Future<String> generateMd5(File file) async {
     List<int> fileBytes = await file.readAsBytes();
     var digest = md5.convert(fileBytes);
     return digest.toString(); // 返回十六进制字符串
   }
-
 }
