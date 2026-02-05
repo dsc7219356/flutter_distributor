1496fb1950ad43bc70219aba85173d94bd83b8fd
dsc <549252873@qq.com>
2026-02-05 09:38:17 +0800

    蒲公英日志加打印

--- packages/flutter_app_publisher/lib/src/publishers/pgyer/publish_pgyer_config.dart
+++ packages/flutter_app_publisher/lib/src/publishers/pgyer/publish_pgyer_config.dart
@@ -43,8 +43,12 @@ class PublishPgyerConfig extends PublishConfig {
       throw PublishError('Missing `$kEnvPgyerApiKey` environment variable.');
     }
     String updatedescription = (environment ?? Platform.environment)[kPGYER_UPDATE_DESCRIPTION_DETAIL]??'';
+    print('参数是这些：${environment ?? Platform.environment}');
+    print('更新描述为：${updatedescription}');
     String appEnv = _parseString(publishArguments?['update-description'])??'';
+    print('环境为：${appEnv}');
     String buildUpdateDescription = appEnv + updatedescription;
+    print('合并后更新描述为：${buildUpdateDescription}');
     return PublishPgyerConfig(
       apiKey: apiKey!,
       oversea: _parseInt(publishArguments?['oversea']),
